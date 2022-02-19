4a559e3fb287c3088f0aaff94a69cf37feaaa04d
bahspeedy <bc313782@gmail.com>
2022-02-19 14:33:22 -0300

    Initial commit

---
+++ aula08.py
@@ -0,0 +1,4 @@
+n1=int(input('digite um valor'))
+n2=int(input('digite outro valor'))
+s = (n1 + n2)/2
+print('A média dos valores é {}'.format(s))
---
+++ ex000.py
@@ -0,0 +1,4 @@
+n=int(input('digite um número:'))
+n2=int(input('digite outro número:'))
+s = n+n2
+print('a soma de {} e {} é {}'.format(n,n2,s))
